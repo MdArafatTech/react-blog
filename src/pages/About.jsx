dbd63b791e425dd4b532f17bc4404a53ba6ca03e
Arafat <mdalarafatabir@gmail.com>
2025-04-28 00:38:30 +0600

    new commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Marqueee from '../components/Marqueee';
 
 import author1 from '../assets/about-author/author1.jpeg';
-import author2 from '../assets/about-author/author2.webp';
+import author2 from '../assets/about-author/author2.png';
 import author3 from '../assets/about-author/author3.jpg';
-import author4 from '../assets/about-author/author4.webp';
+import author4 from '../assets/about-author/author4.jpg';
 import author5 from '../assets/about-author/author5.jpeg';
 import author6 from '../assets/about-author/author6.avif';
 
@@ -24,7 +24,7 @@ const authors = [
     "id": 2,
     "author": "Maureen Heffernan",
     "category": "PARTNERSHIPS & GROUTH DIRECTER",
-    "content": "Shortly after college, Maureen moved to New York to pursue a career in digital marketing, despite her parents’ skepticism that people would ever “buy stuff on the internet.” Since then she has helped grow online sales at companies like L’Oreal and Madewell. Before joining Cup of Jo, she was driving traffic and maximizing ROI on the performance marketing team at J.Crew. Maureen lives in Seattle with her husband and two funny kids. ",
+    "content": "Shortly after college, Maureen moved to New York to pursue a career in digital marketing, despite her parents’  skepticism that people would ever “buy stuff on the internet.” Since then she has helped grow online sales at companies like L’Oreal and Madewell. Before joining Cup of Jo,skepticism that people would ever “buy stuff on the internet.” Since then she has helped grow online sales at companies like L’Oreal and M she was driving traffic and maximizing ROI on the performance marketing team at J.Crew. Maureen lives in Seattle with her husband and two funny kids. ",
     "email": "hello@cupofjo.com",
     "insta": "Instagram",
     "authorImage": author2
@@ -44,7 +44,7 @@ const authors = [
     "id": 4,
     "author": "Kaitlyn Teer",
     "category": "SENIOR NEWSLETTER EDITOR",
-    "content": "Kaitlyn is the newsletter editor for Cup of Jo’s weekly Substack, Big Salad. Her essays on mothering and the climate crisis have appeared in Orion, Catapult, and elsewhere. She has won essay prizes from Fourth Genre and Prairie Schooner. She lives with her spouse and two kids in western Washington, where she enjoys hanging out at the beach and taking family bike rides.",
+    "content": "Kaitlyn is the newsletter editor for Cup of Jo’s weekly Substack, Big Salad. Her essays on mothering and the climate crisis have appeared skepticism that people would ever “buy stuff on the internet.” Since then she has he were  then sh and elsewhere. She has won essay prizes from Fourth Genre and Prairie Schooner. She lives with her spouse and two kids in western Washington, where she enjoys hanging out at the beach and taking family bike rides.",
     "email": "hello@cupofjo.com",
     "insta": "Instagram",
     "authorImage": author4
@@ -64,7 +64,7 @@ const authors = [
     "id": 6,
     "author": "Christine Han",
     "category": "PHOTOGRAPHER",
-    "content": "Christine wears many hats when it comes to writing and books. She’s had a 15-year career in book publishing — working as an editor at Doubleday, Hyperion and Simon and Schuster — where she’s published bestselling books such as From Scratch, Surviving the White Gaze and These Ghosts Are Family. In addition to her passion for championing other writers, she’s a novelist, too. Her most recent novel, You Were Always Mine, written with Jo Piazza, came out in 2023. She loves any chance to talk about race, friendship, books and television, preferably while eating French fries.",
+    "content": "Christine wears many hats when it comes to writing and books. She’s had a 15-year career in book publishing — working as an editor at Doubleday, Hyperion and Simon and Schuster — where she’s published bestselling books such as From Scratch, Surviving the White Gaze and These Ghosts Are Family. In addition to her passion for championing other writers, she’s a novelist, too. Her most recent novel, came out in 2023. She loves any chance to talk about race, friendship, books and television, preferably while eating French fries.",
     "email": "hello@cupofjo.com",
     "insta": "Instagram",
     "authorImage": author6
@@ -77,15 +77,15 @@ const About = () => {
     <div>
       <div className="about">
         <div className='bg-blue-700 text-white text-center pt-5 pb-15 px-[6%]'>
-          <h3 className='text-2xl'>About Cup of Jo</h3>
+          <h3 className='text-2xl'>About MetaBlog</h3>
           <hr className='mt-[20px] mb-[40px]' />
-          <p>Founded in January 2007, Cup of Jo is a daily women’s lifestyle site that covers style, culture, food, travel, relationships and parenting. Known industry wide for its large and deeply engaged readership, Cup of Jo encourages visitors to “come for the blog, stay for the comments.” The site receives over 4 million monthly page views and almost 1 million monthly unique visitors.
+          <p>Founded in April 2025, MetaBlog is a daily bolg making  web site that covers style, travel, relationships and parenting. Known industry wide for its large and deeply engaged readership, MetaBlog encourages visitors to “come for the blog, stay for the comments.” The site receives over 4 million monthly page views and almost 1 million monthly unique visitors.
           <br />
           <br />
-         Cup of Jo regularly donates a percentage of profits to organizations we support, including RAICES, NAACP, NARAL, Greenpeace, Save the Children, Planned Parenthood, the Lilith Fund and The Florence Project. We’re glad you are here. Thank you so much for reading.</p>
+         MetaBlog regularly donates a percentage of profits to organizations we support, including RAICES, NAACP, NARAL, Greenpeace, Save the Children, Planned Parenthood, the Lilith Fund and The Florence Project. We’re glad you are here. Thank you so much for reading.</p>
         </div>
         <h3 className='text-2xl text-center my-[6%]'>Meet Our Team</h3>
-        <div className="author px-[6%] py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+        <div className="author px-[6%] py-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
         
           {authors.map((author) => (
             <div key={author.id} className=" flex flex-col justify-center items-center  ">
@@ -93,7 +93,7 @@ const About = () => {
               <img 
                 src={author.authorImage} 
                 alt={author.author} 
-                className=" h-50 w-50 rounded-[100%] "
+                className="h-[150px] w-[150px] rounded-full "
               />
               <h4 className="text-xl font-bold mb-1">{author.author}</h4>
               <p className="text-sm mb-2">{author.category}</p>
